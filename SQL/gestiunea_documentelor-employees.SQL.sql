d074c3e06fe24fa7e4b0201e4fcd9aa05bad909a
Mihai551 <mihairadu999@gmail.com>
2021-07-14 17:37:45 +0300

    am adaugat codul SQL pentru crearea tabelelor

---
+++ SQL/gestiunea_documentelor-employees.SQL.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `employees` (
+   `legalEntityName` varchar(45) DEFAULT NULL,
+   `firstName` varchar(45) DEFAULT NULL,
+   `lastName` varchar(45) DEFAULT NULL,
+   `pin` varchar(45) DEFAULT NULL,
+   `emailAddress` varchar(45) DEFAULT NULL,
+   `password` varchar(45) DEFAULT NULL,
+   `mystreams` varchar(45) DEFAULT NULL,
+   `streamsForMe` varchar(45) DEFAULT NULL,
+   `inviteCode` varchar(45) DEFAULT NULL
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
