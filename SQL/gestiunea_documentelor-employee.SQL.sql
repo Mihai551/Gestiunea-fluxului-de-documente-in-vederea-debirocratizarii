993c9b5f15d14c1c357c0979bcd7948b23292e9e
Mihai551 <mihairadu999@gmail.com>
2021-07-27 11:06:16 +0300

    schimbarea tabelelor de sign up si login

--- SQL/gestiunea_documentelor-employee.SQL.sql
+++ SQL/gestiunea_documentelor-employee.SQL.sql
@@ -1,11 +1,10 @@
-CREATE TABLE `gestiunea_documentelor`.`employee` (
-   `legalEntityName` varchar(45) DEFAULT NULL,
-   `firstName` varchar(45) DEFAULT NULL,
-   `lastName` varchar(45) DEFAULT NULL,
-   `pin` varchar(45) DEFAULT NULL,
-   `emailAddress` varchar(45) DEFAULT NULL,
-   `password` varchar(45) DEFAULT NULL,
-   `mystreams` varchar(45) DEFAULT NULL,
-   `streamsForMe` varchar(45) DEFAULT NULL,
-   `inviteCode` varchar(45) DEFAULT NULL
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+CREATE TABLE `employee` (
+  `legalEntityEmailAddress` varchar(45) DEFAULT NULL,
+  `firstName` varchar(45) DEFAULT NULL,
+  `lastName` varchar(45) DEFAULT NULL,
+  `pin` varchar(45) DEFAULT NULL,
+  `emailAddress` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT 'employee',
+  KEY `emailAddress_idx` (`emailAddress`),
+  CONSTRAINT `employee_emailAddress` FOREIGN KEY (`emailAddress`) REFERENCES `user` (`emailAddress`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
